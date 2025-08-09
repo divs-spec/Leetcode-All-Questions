a491018c6f291d9f457cea92ad07b6596c24169c
divyani <ikrakizoi2607@gmail.com>
2025-08-09 20:36:41 +0530

    Create Remove Duplicates from Sorted List II.java

---
+++ Leetcode-one-by-one/src/Remove Duplicates from Sorted List II.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        ListNode curr = head;
+        while (curr != null) {
+            freq.put(curr.val, freq.getOrDefault(curr.val, 0) + 1);
+            curr = curr.next;
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        curr = head;
+        while (curr != null) {
+            if (freq.get(curr.val) == 1) {
+                tail.next = new ListNode(curr.val);
+                tail = tail.next;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
