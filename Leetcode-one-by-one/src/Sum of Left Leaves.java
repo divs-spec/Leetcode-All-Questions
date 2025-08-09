56ada591228acd183f3752856a7381cf79785938
divyani <ikrakizoi2607@gmail.com>
2025-08-09 22:33:38 +0530

    Create Sum of Left Leaves.java

---
+++ Leetcode-one-by-one/src/Sum of Left Leaves.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        return dfs(root, false);
+    }
+
+    private int dfs(TreeNode node, boolean isLeft) {
+        if (node == null) return 0;
+        
+        // If it's a leaf
+        if (node.left == null && node.right == null) {
+            return isLeft ? node.val : 0;
+        }
+        
+        // Recursively check left and right children
+        return dfs(node.left, true) + dfs(node.right, false);
+    }
+}
