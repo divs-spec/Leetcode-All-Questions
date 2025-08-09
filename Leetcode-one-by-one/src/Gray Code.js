67e58345d0c37faa4f1855567aa8b443f0dc4b04
divyani <ikrakizoi2607@gmail.com>
2025-08-09 20:50:35 +0530

    Create Gray Code.js

---
+++ Leetcode-one-by-one/src/Gray Code.js
@@ -0,0 +1,8 @@
+var grayCode = function(n) {
+    const result = [];
+    const total = 1 << n;
+    for (let i = 0; i < total; i++) {
+        result.push(i ^ (i >> 1));
+    }
+    return result;
+};
