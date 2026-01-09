b61e2e0497cc0cb30b8752315780c75a51a02453
divyani <ikrakizoi2607@gmail.com>
2026-01-09 19:10:10 +0530

    Create Lowest Common Ancestor of Deepest Leaves.java

---
+++ Leetcode-one-by-one/src/Lowest Common Ancestor of Deepest Leaves.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    private Result dfs(TreeNode node) {
+        if (node == null) {
+            return new Result(0, null);
+        }
+
+        Result left = dfs(node.left);
+        Result right = dfs(node.right);
+
+        if (left.depth == right.depth) {
+            // deepest leaves are in both subtrees
+            return new Result(left.depth + 1, node);
+        } else if (left.depth > right.depth) {
+            return new Result(left.depth + 1, left.node);
+        } else {
+            return new Result(right.depth + 1, right.node);
+        }
+    }
+
+    private static class Result {
+        int depth;
+        TreeNode node;
+
+        Result(int depth, TreeNode node) {
+            this.depth = depth;
+            this.node = node;
+        }
+    }
+}
