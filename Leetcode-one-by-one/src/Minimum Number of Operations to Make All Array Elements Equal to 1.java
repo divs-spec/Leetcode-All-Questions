045c419c4d69fd49dea802e4822ab35144474247
divyani <ikrakizoi2607@gmail.com>
2026-01-15 00:15:33 +0530

    Update Minimum Number of Operations to Make All Array Elements Equal to 1.java

--- Leetcode-one-by-one/src/Minimum Number of Operations to Make All Array Elements Equal to 1.java
+++ Leetcode-one-by-one/src/Minimum Number of Operations to Make All Array Elements Equal to 1.java
@@ -1,1 +1,35 @@
+class Solution {
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public int minOperations(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 1)
+            count++;
+        }
+        if(count > 0)
+        return (nums.length - count);
 
+        int n = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            int g = nums[i];
+            for(int j = i+1; j < nums.length; j++){
+                g = gcd(g,nums[j]);
+                if(g == 1){
+                    n = Math.min(n, j - i);
+                    break;
+                }
+            }
+        }
+        if(n == Integer.MAX_VALUE)
+        return -1;
+        else
+        return (n + nums.length - 1);
+    }
+}
