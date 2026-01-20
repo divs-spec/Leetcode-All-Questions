0ad63b6360b79c48da1721431d4382be7cd6851b
divyani <ikrakizoi2607@gmail.com>
2026-01-20 21:05:02 +0530

    Create Construct the Minimum Bitwise Array I.java

---
+++ Leetcode-one-by-one/src/Construct the Minimum Bitwise Array I.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int p = nums.get(i);
+
+            // Only odd primes except 2 are possible
+            if (p == 2) {
+                ans[i] = -1;
+                continue;
+            }
+
+            // Count trailing 1s
+            int k = 0;
+            int temp = p;
+            while ((temp & 1) == 1) {
+                k++;
+                temp >>= 1;
+            }
+
+            // Minimum x
+            ans[i] = p - (1 << (k - 1));
+        }
+
+        return ans;
+    }
+}
