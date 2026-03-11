0f5951a857b956f0e89f06d7d9079cf10ccd27f2
divyani <ikrakizoi2607@gmail.com>
2026-03-12 00:19:56 +0530

    Add function to compute bitwise complement of integer

---
+++ Leetcode-one-by-one/src/Complement of Base 10 Integer.go
@@ -0,0 +1,13 @@
+func bitwiseComplement(n int) int {
+    if n == 0 {
+        return 1
+    }
+
+    mask := 1
+    for mask <= n {
+        mask <<= 1
+    }
+
+    mask -= 1
+    return n ^ mask
+}
