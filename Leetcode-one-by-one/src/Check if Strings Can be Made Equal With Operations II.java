d24ea8584ab3985b6a999991a2f18c2f294ebc91
divyani <ikrakizoi2607@gmail.com>
2026-03-30 22:08:57 +0530

    Create Check if Strings Can be Made Equal With Operations II.java

---
+++ Leetcode-one-by-one/src/Check if Strings Can be Made Equal With Operations II.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkStrings(String s1, String s2) {
+		int[][] freq = new int[2][26];
+		for (int i = 0; i < s1.length(); i++) {
+			freq[i % 2][s1.charAt(i) - 'a']++;
+			freq[i % 2][s2.charAt(i) - 'a']--;
+		}
+		for (int i = 0; i < 26; i++) {
+			if (freq[0][i] != 0 || freq[1][i] != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
