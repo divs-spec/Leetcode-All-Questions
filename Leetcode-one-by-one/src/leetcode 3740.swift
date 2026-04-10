93bc922deed5d12eaa60e835013f9c309846e1a2
divyani <ikrakizoi2607@gmail.com>
2026-04-10 22:27:55 +0530

    Create leetcode 3740.swift

---
+++ Leetcode-one-by-one/src/leetcode 3740.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func minimumDistance(_ nums: [Int]) -> Int {
+        let n = nums.count
+        var ans = n + 1
+        
+        for i in 0..<(n - 2) {
+            for j in (i + 1)..<(n - 1) {
+                if nums[i] != nums[j] { continue }
+                
+                for k in (j + 1)..<n {
+                    if nums[j] == nums[k] {
+                        ans = min(ans, k - i)
+                        break
+                    }
+                }
+            }
+        }
+        
+        return ans == n + 1 ? -1 : ans * 2
+    }
+}
